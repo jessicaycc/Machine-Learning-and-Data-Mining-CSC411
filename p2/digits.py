aff54dae30129ea54f90d11c8a6dd0f47a5df2a9
jessicaycc <jessicaycc.chen@mail.utoronto.ca>
2018-02-11 13:35:09 -0500

    genx

--- p2/digits.py
+++ p2/digits.py
@@ -5,6 +5,15 @@ from scipy.io import loadmat
 
 np.random.seed(0)
 M = loadmat("mnist_all.mat")
+print np.shape(M)
 
-X = genX(M)
-Y = genY(M)
+def genX():
+    Matrix = np.empty((0,784), float)
+    for i in range (0,10):
+        for j in range (SET_RATIO[0]):
+            filename = "train" + str(i)
+            x =np.array([ M[filename][j].reshape(IMG_SHAPE).flatten() / 255. ])
+            Matrix = np.vstack((Matrix, x))
+    return Matrix
+X = genX()
+#Y = genY(M)
