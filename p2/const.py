21159b03ccdc69ef5f515dd8cdf0dd62076ba89f
jessicaycc <jessicaycc.chen@mail.utoronto.ca>
2018-02-11 11:03:14 -0500

    Added Gradient Descent... dont think it works tho

--- p2/const.py
+++ p2/const.py
@@ -2,3 +2,5 @@ import numpy as np
 
 EPS = 1e-5
 IMG_SHAPE = (28, 28)
+LRN_RATE = 0.01
+MAX_ITER = 300000
--- p2/digits.py
+++ p2/digits.py
@@ -37,6 +37,25 @@ def relativeError(a, b):
     a, b = abs(a), abs(b)
     return 2*abs(a-b) / float(a+b)
 
+def gradDescent(f, df, x, w0, y, p, alpha=LRN_RATE, intr=5000, maxIter=MAX_ITER, out=True):
+    def update(f, df, x, y, p, i):
+        print "Iter", i
+        print "J(x) =", f(y, p) 
+        print "Gradient:", df(x, y, p), "\n"
+        return
+
+    i = 0
+    W = w0.copy()
+    wPrev = w0 - 10*EPS
+    while norm(W - wPrev) > EPS and i < maxIter:
+        wPrev = W.copy()
+        W -= alpha * df(x, y, p)
+        if i % intr == 0 and out:
+            update(f, df, x, y, p, i)
+        i += 1
+    if out:
+        update(f, df, x, y, p, i)
+    return W
 
 np.random.seed(0)
 M = loadmat("mnist_all.mat")
@@ -49,6 +68,9 @@ b = np.random.rand(len(y), 1)
 n = dC_weight(x, y, forward(x, w, b))
 m = finiteDiff_weight(x, w, b, y, 153, 5)
 print relativeError(n[153][5], m)
+w0 = np.zeros((len(x), len(y)))
+print (gradDescent(C, dC_weight, x, w0, y, forward(x ,w0, b)))
+
 
 n = dC_bias(y, forward(x, w, b))
 m = finiteDiff_bias(x, w, b, y, 1)
