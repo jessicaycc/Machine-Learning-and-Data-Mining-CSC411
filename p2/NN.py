softmax fixed

p2/NN.py
@@ -1,7 +1,7 @@
 from const import *
 
 def softmax(O):
-    return exp(O)/np.tile(sum(exp(O),0), (len(O),1))
+    return exp(O)/exp(O).sum(axis=1, keepdims=True)
 
 def forward(X, W, b):
     i = np.ones((len(X), 1))

p2/const.py
@@ -12,5 +12,5 @@ SET_RATIO = (100, 10, 10)
 LABEL = np.identity(10)
 
 EPS = 1e-5
-LRN_RATE = 0.1
+LRN_RATE = 0.025
 MAX_ITER = 30000

p2/digits.py
@@ -1,6 +1,4 @@
-from const import *
 from calc import *
-from NN import *
 from scipy.io import loadmat
 
 np.random.seed(0)
@@ -8,15 +6,17 @@ M = loadmat("mnist_all.mat")
 
 X = genX(M)
 Y = genY(SET_RATIO[0])
-W = np.random.rand(NUM_LABEL, NUM_FEAT)
-b = np.random.rand(NUM_LABEL, 1)
+W = np.zeros((NUM_LABEL, NUM_FEAT))
+b = np.zeros((NUM_LABEL, 1))
 
-P = forward(X, W, b)
+#P = forward(X, W, b)
 
-n = dC_weight(X, Y, P)
-m = finiteDiff_weight(X, Y, W, b, 0, 38)
-print relativeError(n[0][38], m)
+#n = dC_weight(X, Y, P)
+#m = finiteDiff_weight(X, Y, W, b, 5, 157)
+#print relativeError(n[5][157], m)
 
-n = dC_bias(X, Y, P)
-m = finiteDiff_bias(X, Y, W, b, 1)
-print relativeError(n[1][0], m)
+#n = dC_bias(X, Y, P)
+#m = finiteDiff_bias(X, Y, W, b, 1)
+#print relativeError(n[1][0], m)
+
+W, b = gradDescent(X, Y, W, b)