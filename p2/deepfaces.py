89f25d968132f558184e56455a6bb534822111ec
Hunter <hunter.richards@mail.utoronto.ca>
2018-02-23 00:42:01 -0500

    new images and compatible with py36

--- p2/deepfaces.py
+++ p2/deepfaces.py
@@ -1,98 +1,125 @@
 import torch
 import torchvision
+import numpy as np
 import torch.nn as nn
 import torchvision.models as models
-from const import *
 from plot import *
 from getdata import *
 from torch.autograd import Variable
 from scipy.misc import imread, imresize
 
 torch.manual_seed(0)
-act = ["Lorraine Bracco", "Peri Gilpin", "Angie Harmon", "Alec Baldwin", "Bill Hader", "Steve Carell"]
+act = ['Lorraine Bracco', 'Peri Gilpin', 'Angie Harmon', 'Alec Baldwin', 'Bill Hader', 'Steve Carell']
+
+class MyAlexNet(nn.Module):
+    def load_weights(self):
+        an_builtin = torchvision.models.alexnet(pretrained=True)
+        
+        self.conv1[0].weight = an_builtin.features[0].weight
+        self.conv1[0].bias = an_builtin.features[0].bias
+
+        self.conv2[0].weight = an_builtin.features[3].weight
+        self.conv2[0].bias = an_builtin.features[3].bias
+
+        self.conv3[0].weight = an_builtin.features[6].weight
+        self.conv3[0].bias = an_builtin.features[6].bias
+
+        self.conv4[0].weight = an_builtin.features[8].weight
+        self.conv4[0].bias = an_builtin.features[8].bias
+
+        self.conv5[0].weight = an_builtin.features[10].weight
+        self.conv5[0].bias = an_builtin.features[10].bias
+
+        classifier_weight_i = [1, 4, 6]
+        for i in classifier_weight_i:
+            self.classifier[i].weight = an_builtin.classifier[i].weight
+            self.classifier[i].bias = an_builtin.classifier[i].bias
+
+    def __init__(self, num_classes=1000):
+        super(MyAlexNet, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        
+        self.load_weights()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        #x = self.classifier(x)
+        return x
 
 #______________________________ PART 10 _____________________________#
 def part10():
-    class MyAlexNet(nn.Module):
-        def load_weights(self):
-            an_builtin = torchvision.models.alexnet(pretrained=True)
-            
-            features_weight_i = [0, 3, 6, 8, 10]
-            for i in features_weight_i:
-                self.features[i].weight = an_builtin.features[i].weight
-                self.features[i].bias = an_builtin.features[i].bias
-                
-            classifier_weight_i = [1, 4, 6]
-            for i in classifier_weight_i:
-                self.classifier[i].weight = an_builtin.classifier[i].weight
-                self.classifier[i].bias = an_builtin.classifier[i].bias
 
-        def __init__(self, num_classes=1000):
-            super(MyAlexNet, self).__init__()
-            self.features = nn.Sequential(
-                nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=3, stride=2),
-                nn.Conv2d(64, 192, kernel_size=5, padding=2),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=3, stride=2),
-                nn.Conv2d(192, 384, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(384, 256, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=3, stride=2),
-            )
-            self.classifier = nn.Sequential(
-                nn.Dropout(),
-                nn.Linear(256 * 6 * 6, 4096),
-                nn.ReLU(inplace=True),
-                nn.Dropout(),
-                nn.Linear(4096, 4096),
-                nn.ReLU(inplace=True),
-                nn.Linear(4096, num_classes),
-            )
-            
-            self.load_weights()
+    # convert images to 227x227
+#    getData(act, (227, 227), download=False)
 
-        def forward(self, x):
-            x = self.features(x)
-            x = x.view(x.size(0), 256 * 6 * 6)
-            x = self.classifier(x)
-            return x
-
-    # model_orig = torchvision.models.alexnet(pretrained=True)
+    # init model
     model = MyAlexNet()
     model.eval()
 
     # read an image
-    im = imread('kiwi227.png')[:,:,:3]
+    im = imread('processed/227x227/alec_baldwin1.jpg')[:,:,:3]
     im = im - np.mean(im.flatten())
     im = im/np.max(np.abs(im.flatten()))
-    im = np.rollaxis(im, -1).astype(float32)
+    im = np.rollaxis(im, -1).astype(np.float32)
 
     # turn the image into a numpy variable
-    im_v = Variable(torch.from_numpy(im).unsqueeze_(0), requires_grad=False)    
+    im_v = Variable(torch.from_numpy(im).unsqueeze_(0), requires_grad=False)
 
     # run the forward pass AlexNet prediction
     softmax = nn.Softmax()
     all_probs = softmax(model.forward(im_v)).data.numpy()[0]
     sorted_ans = np.argsort(all_probs)
 
-    for i in range(-1, -6, -1):
-        print("Answer:", class_names[sorted_ans[i]], ", Prob:", all_probs[sorted_ans[i]])
-
-    ans = np.argmax(model.forward(im_v).data.numpy())
-    prob_ans = softmax(model.forward(im_v)).data.numpy()[0][ans]
-    print("Top Answer:", class_names[ans], "P(ans) = ", prob_ans)
+#    for i in range(-1, -6, -1):
+#        print('Answer:', class_names[sorted_ans[i]], ', Prob:', all_probs[sorted_ans[i]])
+#
+#    ans = np.argmax(model.forward(im_v).data.numpy())
+#    prob_ans = softmax(model.forward(im_v)).data.numpy()[0][ans]
+#    print('Top Answer:', class_names[ans], 'P(ans) = ', prob_ans)
     return
 
 #_______________________________ MAIN _______________________________#
-if __name__ == "__main__":
+if __name__ == '__main__':
     start = time.time()
 
     part10()
 
     end = time.time()
-    print("Time elapsed:", end-start)
+    print('Time elapsed:', end-start)
--- p2/getData.py
+++ p2/getData.py
@@ -2,100 +2,109 @@ import hashlib
 import threading
 from const import *
 from PIL import Image
-#from urllib import urlretrieve
 from urllib.request import urlretrieve
 
-if not os.path.exists("original"):
-    os.makedirs("original")
+if not os.path.exists('original'):
+    os.makedirs('original')
 
-if not os.path.exists("processed"):
-    os.makedirs("processed")
+if not os.path.exists('processed'):
+    os.makedirs('processed')
 
 def timeout(func, args=(), kwargs={}, timeout_duration=10, default=None):
     class InterruptableThread(threading.Thread):
         def __init__(self):
             threading.Thread.__init__(self)
             self.result = None
+
         def run(self):
             try:
                 self.result = func(*args, **kwargs)
             except IOError:
                 self.result = default
+
     it = InterruptableThread()
     it.start()
     it.join(timeout_duration)
+
     if it.isAlive():
         return(False)
     else:
         return(it.result)
 
-def process(filename, data):
-    bounding_box = tuple(map(int, data[DATA_BBOX].split(",")))
-    with open("original/"+filename, "rb") as infile:
+def process(filename, data, res):
+    bounding_box = tuple(map(int, data[DATA_BBOX].split(',')))
+
+    with open('original/' + filename, 'rb') as infile:
         hash_matches = data[DATA_HASH] == hashlib.sha256(infile.read()).hexdigest()
-    img = Image.open("original/"+filename).crop(bounding_box).resize(DATA_SIZE)
-    is_coloured = img.mode not in ("L", "LA")
+    img = Image.open('original/' + filename).crop(bounding_box).resize(res)
+    is_coloured = img.mode not in ('L', 'LA')
+
     if hash_matches and is_coloured:
-        img.save("processed/" + filename)
+        dir = '{}x{}'.format(res[0], res[1])
+        if not os.path.exists('processed/'+dir):
+            os.makedirs('processed/'+dir)
+        img.save('processed/{}/{}'.format(dir, filename))
     else:
-        err_msg = "hash does not match" if not hash_matches else "image has no colour"
+        err_msg = 'hash does not match' if not hash_matches else 'image has no colour'
         raise IOError(err_msg)
+
     return
 
-def getData(act, download=True):
-    def search(name, textFile, download):
-        i = 0
-        for line in open(textFile):
-            if name in line:
-                data = line.strip("\r\n").split("\t")
-                ext = "." + data[DATA_URL].split(".")[-1]
-                filename = data[DATA_NAME].replace(" ","_").lower() + str(i) + ext
-                if download:
-                    timeout(urlretrieve, (data[DATA_URL], "original/"+filename), {})
-                if os.path.isfile("original/"+filename):
-                    try:
-                        process(filename, data)
-                        print(filename, "- success")
-                    except IOError as err:
-                        print("{} - failed with error: {}".format(filename, err.args[0]))
-                else:
-                    print(filename, "- failed with error: image failed to download")
-                i += 1
-        return
+def search(name, textFile, res, download):
+    for i, line in enumerate(l for l in open(textFile) if name in l):
+        data = line.strip('\r\n').split('\t')
+        ext = '.' + data[DATA_URL].split('.')[-1]
+        filename = data[DATA_NAME].replace(' ', '_').lower() + str(i) + ext
+
+        if download:
+            timeout(urlretrieve, (data[DATA_URL], 'original/'+filename), {})
+
+        if os.path.isfile('original/'+filename):
+            try:
+                process(filename, data, res)
+                print(filename, '- success')
+            except IOError as err:
+                print('{} - failed with error: {}'.format(filename, err.args[0]))
+        else:
+            print(filename, '- failed with error: image failed to download')
+
+    return
+
+def getData(act, res, download=True):
     for name in act:
-        search(name, "data/facescrub_actors.txt", download)
-        search(name, "data/facescrub_actresses.txt", download)
+        search(name, 'data/facescrub_actors.txt', res, download)
+        search(name, 'data/facescrub_actresses.txt', res, download)
     return
 
-def getSets(act, set_ratio=DATA_SET_RATIO):
-    set_ratio = [float(x)/sum(set_ratio) for x in set_ratio]
+def getSets(act, set_ratio, dir):
     set1, set2, set3 = list(), list(), list()
+    set_ratio = [float(x)/sum(set_ratio) for x in set_ratio]
     for name in act:
-        filename = name.replace(" ","_").lower()
-        database = [f for f in os.listdir("processed") if f.startswith(filename)]
+        filename = name.replace(' ', '_').lower()
+        database = [f for f in os.listdir('processed/'+dir) if f.startswith(filename)]
         size = [int(x*len(database)) for x in set_ratio]
         sample = np.random.choice(database, sum(size), replace=False)
         set1.append(sample[:size[0]])
         set2.append(sample[size[0]:size[0]+size[1]])
         set3.append(sample[size[0]+size[1]:])
-    return(set1, set2, set3)
+    return set1, set2, set3
 
-def genX(file_set):
-    X = np.empty((0, IN_SIZE), float)
+def genX(file_set, num_features, dir):
+    X = np.empty((0, num_features), float)
     for file_list in file_set:
         for filename in file_list:
-            img = Image.open("processed/"+filename)
+            img = Image.open('processed/{}/{}'.format(dir, filename))
             x = np.array(img)[:,:,:3].flatten() / 255.
             X = np.vstack((X, x))
-    return( convert(X, (0,1), (-1,1)) )
+    return X
 
-def genY(file_set):
-    labels = np.identity(OUT_SIZE)
-    Y = np.empty((0, OUT_SIZE), float)
+def genY(file_set, num_labels):
+    labels = np.identity(num_labels)
+    Y = np.empty((0, num_labels), float)
     for i, file_list in enumerate(file_set):
         size = len(file_list)
         Y = np.vstack(( Y, np.tile(labels[i], (size, 1)) ))
-    return( convert(Y, (0,1), (-1,1)) )
+    return Y
 
 def convert(A, range_old, range_new):
     len_old = range_old[1]-range_old[0]
