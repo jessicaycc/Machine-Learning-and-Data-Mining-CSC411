a1d3182e1db368696518a62f7ed4c13342219b6d
jessicaycc <jessicaycc.chen@mail.utoronto.ca>
2018-03-11 14:20:50 -0400

    Part 3 done

--- p3/fake.py
+++ p3/fake.py
@@ -1,5 +1,9 @@
-from getdata import *
+import math
+import operator
+import numpy as np
+from functools import reduce
 
+from getdata import *
 #______________________________ PART 1 ______________________________#
 def part1():
     train, valid, test = (a+b for a,b in zip(genSets('clean_real.txt'), genSets('clean_fake.txt')))
@@ -17,10 +21,42 @@ def part1():
 
 #______________________________ PART 2 ______________________________#
 def part2():
+    train_x = loadObj("train_x")
+    valid_x = loadObj("valid_x")
+    naiveBayes(train_x, valid_x, trainSet = True)
+    naiveBayes(train_x,valid_x, trainSet = False)
     return
 
 #______________________________ PART 3 ______________________________#
 def part3():
+    train_x = loadObj("train_x")
+    
+    m = 7
+    p = 0.03
+    realSize = int(1968*DATA_SET_RATIO[0])
+    fakeSize = int(1298*DATA_SET_RATIO[0])
+    pReal = realSize/(realSize+fakeSize)
+    pFake = fakeSize/(realSize+fakeSize)
+    invertTrain_x = -1*(train_x - 1)
+    real_x1 = ((np.sum(train_x[:realSize],axis=0))+m*p)/(realSize+m)
+    fake_x1 = ((np.sum(train_x[realSize:],axis=0))+m*p)/(fakeSize+m)
+    real_x0 = ((np.sum(invertTrain_x[:realSize],axis=0))+m*p)/(realSize+m)
+    fake_x0 = ((np.sum(invertTrain_x[realSize:],axis=0))+m*p)/(fakeSize+m)
+    real_presence = np.divide(list(map(lambda x: x * pReal, real_x1)), (real_x1*pReal+fake_x1*pFake))
+    real_absence = np.divide(list(map(lambda x: x * pReal, real_x0)), (real_x0*pReal+fake_x0*pFake))
+    fake_presence = np.divide(list(map(lambda x: x * pFake, fake_x1)), (real_x1*pReal+fake_x1*pFake))
+    fake_absence = np.divide(list(map(lambda x: x * pFake, fake_x0)), (real_x0*pReal+fake_x0*pFake))
+    print ("TOP 10 WORDS:")
+    getTop10(real_presence,10)
+    getTop10(real_absence,10) 
+    getTop10(fake_presence,10) 
+    getTop10(fake_absence,10)
+    print ("\nTOP 10 NON-STOPWORDS:")
+    getTop10_noStop(real_presence,10)
+    getTop10_noStop(real_absence,10) 
+    getTop10_noStop(fake_presence,10) 
+    getTop10_noStop(fake_absence,10)
+
     return
 
 #______________________________ PART 4 ______________________________#
@@ -47,14 +83,14 @@ def part8():
 if __name__ == '__main__':
     start = time.time()
 
-    part1()
+    #part1()
     #part2()
-    #part3()
+    part3()
     #part4()
     #part5()
     #part6()
     #part7()
     #part8()
-
+  
     end = time.time()
     print('Time elapsed:', end-start)
--- p3/getdata.py
+++ p3/getdata.py
@@ -1,3 +1,7 @@
+import math
+import operator
+import numpy as np
+from functools import reduce
 from const import *
 
 def genSets(input, ratio=DATA_SET_RATIO):
@@ -28,3 +32,60 @@ def genX(input, vocab):
             if word in vocab:
                 X[i][vocab[word]] = 1.
     return X
+
+def naiveBayes(train_x, valid_x, trainSet = True):
+    count = 0
+    m = 7
+    p = 0.03
+    realSize = int(1968*DATA_SET_RATIO[0])
+    fakeSize = int(1298*DATA_SET_RATIO[0])
+    pReal = realSize/(realSize+fakeSize)
+    pFake = fakeSize/(realSize+fakeSize)
+    real = ((np.sum(train_x[:realSize],axis=0))+m*p)/(realSize+m)
+    fake = ((np.sum(train_x[realSize:],axis=0))+m*p)/(fakeSize+m)
+    if trainSet:
+        midpoint = int(1968*DATA_SET_RATIO[0])
+        set = train_x
+        total = 2285.0
+    else:
+        midpoint = int(1968*DATA_SET_RATIO[1])
+        set = valid_x
+        total = 489.0
+    for j, line in enumerate(set):
+        realLst = [real[i] for i, n in enumerate(line) if n == 1]
+        realLst = list(map(lambda x: math.log10(x),realLst))
+        realLst = exp(reduce(operator.add,realLst))
+        fakeLst = [fake[i] for i, n in enumerate(line) if n == 1]
+        fakeLst = list(map(lambda x: math.log10(x),fakeLst))
+        fakeLst = exp(reduce(operator.add,fakeLst))
+        predReal = realLst*pReal
+        predFake = fakeLst*pFake
+        pred = 1 if predFake > predReal else 0
+        if j < midpoint:
+            if pred ==0:
+                count+=1
+        else:
+            if pred ==1:
+                count+=1
+    accuracy = (count/total)*100
+    print ("Accuracy:", accuracy, "%")
+    return accuracy
+
+def getTop10(array, top):
+    vocab = loadObj("vocab")
+    vocab = list(vocab.keys())
+    index = np.argpartition(array, -top)[-top:]
+    index = index[np.argsort(array[index])]
+    top10 = [vocab[i] for i in index] 
+    print(top10)
+    return top10
+
+def getTop10_noStop(array, top):
+    from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+    vocab = loadObj("vocab")
+    vocab = list(vocab.keys())
+    index = np.argsort(array)
+    topWords = [vocab[i] for i in (index) if vocab[i] not in ENGLISH_STOP_WORDS]
+    top10 = topWords[-top:]
+    print (top10)
+    return top10
